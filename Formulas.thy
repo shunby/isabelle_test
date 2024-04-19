bb2562ba3995d8f3fe134ea8330a603165af3d0d
shunby <shunby.h@gmail.com>
2024-04-19 18:50:40 +0900

    abc

--- Formulas.thy
+++ Formulas.thy
@@ -48,5 +48,68 @@ lemma "\<lbrakk> xs @ ys = ys @ xs; length xs = length ys \<rbrakk> \<Longrighta
 lemma "\<lbrakk> (a::nat) \<le> b; b \<le> c; c \<le> d; d \<le> e \<rbrakk> \<Longrightarrow> a \<le> e"
   by (blast intro: le_trans)
 
+thm conjI [OF refl[of "a"] refl[of "b"]]
 
+lemma "Suc (Suc(Suc a)) \<le> b \<Longrightarrow> a \<le> b"
+  by (blast dest:Suc_leD)
+
+inductive ev :: "nat \<Rightarrow> bool" where
+ev0: "ev 0" |
+evSS: "ev n \<Longrightarrow> ev (Suc(Suc(n)))"
+
+fun evn :: "nat \<Rightarrow> bool" where
+"evn 0 = True" |
+"evn (Suc 0) = False" |
+"evn (Suc(Suc n)) = evn n"
+declare ev.intros[simp, intro]
+
+lemma "ev (Suc(Suc(Suc(Suc 0))))"
+  apply (rule evSS)
+  apply (rule evSS)
+  apply (rule ev0)
+  done
+
+lemma "ev m \<Longrightarrow> evn m"
+  apply (induction rule:ev.induct)
+  by (simp_all)
+
+lemma "evn n \<Longrightarrow> ev n"
+  apply (induction n rule: evn.induct)
+  by (simp_all add: ev0 evSS)
+
+  
+inductive star :: "('a\<Rightarrow>'a\<Rightarrow>bool) \<Rightarrow> 'a\<Rightarrow>'a\<Rightarrow>bool" for r where
+  refl: "star r x x" |
+  step: "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+
+lemma star_trans: "star r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+  apply (induction rule:star.induct)
+  apply(assumption)
+  apply (metis step)
+  done
+
+inductive palindrome :: "'a list \<Rightarrow> bool" where
+  pal_nil: "palindrome []" |
+  pal_cons: "palindrome xs \<Longrightarrow> palindrome (a # xs @ [a])"
+
+theorem "palindrome xs \<Longrightarrow> rev xs = xs"
+  apply (induction rule: palindrome.induct)
+  apply simp
+  apply simp
+  done
+
+inductive star' :: "('a\<Rightarrow>'a\<Rightarrow>bool) \<Rightarrow> 'a\<Rightarrow>'a\<Rightarrow>bool" for r where
+  refl': "star' r x x" |
+  step': "star' r x y \<Longrightarrow> r y z \<Longrightarrow> star' r x z"
+
+lemma star_refl: "star r x y \<Longrightarrow> star r y x"
+  apply (induction rule: star.induct)
+  apply (auto simp add: refl step star_trans)
+
+
+lemma "star' r x y \<Longrightarrow> star r x y"
+  apply (induction rule:star'.induct)
+  apply (simp add:refl)
+  apply (simp add:step step')
+  
 end
